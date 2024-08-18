1e0f554708685478664ac335d8de5829b763c50f
sussa3007 <sussa1933@gmail.com>
2024-08-19 02:03:20 +0900

    Add deployment scripts and Docker configuration
    This commit introduces `dev-deploy.sh`, a script to automate the deployment of the development build with Docker. Additionally, it includes a `Dockerfile` and `docker-compose.yml` for containerization and orchestration of the application, exposing it on port 8987. These changes streamline the deployment process and improve environment consistency.

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -32,17 +32,17 @@ sudo cp $BUILD_JAR $EXIST_FILE
 APP_NAME=marketduck
 CONTAINER_NAME=marketduck-container
 # Step 3: Stop and remove existing container if it exists
-if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+if [ "$(sudo docker ps -q -f name=$CONTAINER_NAME)" ]; then
     echo "Stopping existing container..."
-    docker stop $CONTAINER_NAME
+    sudo docker stop $CONTAINER_NAME
     echo "Removing existing container..."
-    docker rm $CONTAINER_NAME
+    sudo docker rm $CONTAINER_NAME
 fi
 
 echo "> Dev DEPLOY_JAR 배포"    >> $LOG_PATH/deploy.log
 
 # Step 4: Build Docker image
-docker-compose build
+sudo docker-compose build
 
 # Step 5: Run Docker containers using Docker Compose
-docker-compose up -d
+sudo docker-compose up -d
