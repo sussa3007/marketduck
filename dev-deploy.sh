5ea7e4b9ecf38987245818fe4ac448892fde6b3f
sussa3007 <sussa1933@gmail.com>
2024-08-19 03:11:26 +0900

    Add deployment scripts and Docker configuration
    This commit introduces `dev-deploy.sh`, a script to automate the deployment of the development build with Docker. Additionally, it includes a `Dockerfile` and `docker-compose.yml` for containerization and orchestration of the application, exposing it on port 8987. These changes streamline the deployment process and improve environment consistency.

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -33,12 +33,44 @@ sudo cp $BUILD_JAR $EXIST_FILE
 
 APP_NAME=marketduck
 CONTAINER_NAME=marketduck-app
+
+# MySQL 컨테이너 이름
+MYSQL_CONTAINER_NAME=mysql
+
+# Redis 컨테이너 이름
+REDIS_CONTAINER_NAME=redis_boot
+REDIS_DEV_CONTAINER_NAME=redis_boot_dev
+
 # Step 3: Stop and remove existing container if it exists
 if [ "$(sudo docker ps -q -f name=$CONTAINER_NAME)" ]; then
     echo "Stopping existing container..."
     sudo docker stop $CONTAINER_NAME
     echo "Removing existing container..."
     sudo docker rm $CONTAINER_NAME
+fi
+
+# 기존 MySQL 컨테이너를 중지하고 제거
+if [ "$(docker ps -q -f name=$MYSQL_CONTAINER_NAME)" ]; then
+    echo "Stopping existing MySQL container..."
+    docker stop $MYSQL_CONTAINER_NAME
+    echo "Removing existing MySQL container..."
+    docker rm $MYSQL_CONTAINER_NAME
+fi
+
+# 기존 Redis 컨테이너를 중지하고 제거
+if [ "$(docker ps -q -f name=$REDIS_CONTAINER_NAME)" ]; then
+    echo "Stopping existing Redis container..."
+    docker stop $REDIS_CONTAINER_NAME
+    echo "Removing existing Redis container..."
+    docker rm $REDIS_CONTAINER_NAME
+fi
+
+# 기존 Redis Dev 컨테이너를 중지하고 제거
+if [ "$(docker ps -q -f name=$REDIS_DEV_CONTAINER_NAME)" ]; then
+    echo "Stopping existing Redis Dev container..."
+    docker stop $REDIS_DEV_CONTAINER_NAME
+    echo "Removing existing Redis Dev container..."
+    docker rm $REDIS_DEV_CONTAINER_NAME
 fi
 
 echo "> Dev DEPLOY_JAR 배포"    >> $LOG_PATH/deploy.log
