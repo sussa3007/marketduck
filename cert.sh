29cd89c6e8c7f545ed83f69130d12f4a735e6985
sussa3007 <sussa1933@gmail.com>
2024-08-31 11:25:29 +0900

    Remove commented-out virtual host configs and add mobile agent handling
    The old commented-out virtual host configurations were removed to clean up the file. Added logic to handle mobile user agents and a new location block for server-sent events with specific proxy headers.

--- cert.sh
+++ cert.sh
@@ -1,8 +1,7 @@
 
 docker-compose run --rm --entrypoint "\
 certbot certonly \
--d api.greneta.shop \
--d blender.greneta.shop \
+-d marketduck.server-su.site \
 --email sussa@gmail.com \
 --manual --preferred-challenges dns \
 --server https://acme-v02.api.letsencrypt.org/directory \
